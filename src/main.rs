fd35c413916d855ab620d918b5362efc1b2115c5
Jill Please <jill@jillplease.de>
2023-11-02 19:21:05 +0100

    basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,75 @@
-use warp::Filter;
+use hyper::body::Bytes;
+use hyper::HeaderMap;
+use std::collections::HashMap;
 use std::convert::Infallible;
+use std::io;
 use std::io::{stdin, Write};
-use std::net::SocketAddr;
-use std::sync::{Arc, mpsc};
-use std::sync::mpsc::{Receiver, Sender};
-use std::{io, thread};
-use hyper::{Body, Request, Response, Server};
-use hyper::body::{Bytes, HttpBody};
-use hyper::service::{make_service_fn, service_fn};
-use urlencoding::decode;
-use futures::executor::block_on;
-use tokio::sync::Mutex;
+use std::sync::mpsc::Sender;
+use std::sync::{mpsc, Arc};
+use warp::Filter;
 
 #[derive(Clone)]
 struct Senders {
-    pub cli_sender : Arc<Sender<String>>,
-    pub timer_sender : Arc<Sender<String>>,
-    pub server_sender : Arc<Sender<String>>,
+    pub cli: Arc<Sender<String>>,
+    pub timer: Arc<Sender<String>>,
+    pub server: Arc<Sender<String>>,
 }
 
 unsafe impl Send for Senders {}
 
 fn with_senders(
     sender: Senders,
-) -> impl Filter<Extract = (Senders,), Error = std::convert::Infallible> + Clone
-{
+) -> impl Filter<Extract = (Senders,), Error = std::convert::Infallible> + Clone {
     warp::any().map(move || sender.clone())
 }
 
-async fn handle(req: Bytes, senders : Senders) -> Result<String, String> {
+fn handle_twitch_event(
+    headers: HeaderMap,
+    body: serde_json::Value,
+    senders: Senders,
+) -> Result<String, String> {
+    let body = if let Some(b) = body.as_object() {
+        b
+    } else {
+        return Err("".to_string());
+    };
+    let event = body.get("event").unwrap().as_object().unwrap();
+    println!("\n\n{:?}", body.keys().collect::<Vec<_>>());
+    println!("\n\n{:?}", event.keys().collect::<Vec<_>>());
     Ok("".to_string())
 }
 
-async fn server(ip: [u8; 4], port: u16, senders: Senders) {
-    println!("aaaa");
-    let routes = warp::any()
-        .and(warp::body::bytes())
-        .and(with_senders(senders.clone()))
-        .and_then( move | body, senders| async move {
-        Ok::<_, Infallible>(handle(body, senders).await)
-    });
-    warp::serve(routes).run((ip, port)).await;
-    //
-    //
-    //
-    // println!("aaaa");
-    // let addr = SocketAddr::from((ip, port));
-    // let make_service = make_service_fn(move |_conn| async move {
-    //     Ok::<_, Infallible>(service_fn(move |req| handle(req, senders.clone())))
-    // });
-    // let server = Server::bind(&addr).serve(make_service);
-    // if let Err(e) = server.await {
-    //     eprintln!("server error: {}", e);
-    // }
+fn handle_kofi_event() {}
+
+async fn handle_url_encoded(headers: HeaderMap, body: HashMap<String, String>, senders: Senders) -> Result<String, String>{
+    println!("{:?}", body);
+    Ok("".to_string())
 }
 
-async fn timer(settings : bool, server_tx: Sender<String>, cli_tx : Sender<String>, rx: Receiver<String>) {
-    let _ = cli_tx.send("".to_string());
-    println!("aaaa");
+async fn handle_json(
+    headers: HeaderMap,
+    body: serde_json::Value,
+    senders: Senders,
+) -> Result<String, String> {
+    handle_twitch_event(headers, body, senders)
+}
+
+async fn server(ip: [u8; 4], port: u16, senders: Senders) {
+    println!("Starting server...");
+    let routes = warp::any()
+        .and(warp::header::headers_cloned())
+        .and(warp::body::json())
+        .and(with_senders(senders.clone()))
+        .and_then(move |headers, body, senders| async move {
+            Ok::<_, Infallible>(handle_json(headers, body, senders).await)
+        });
+    let _ = senders.cli.send("server".to_string());
+    println!("{:?}", warp::serve(routes).run((ip, port)).await);
+}
+
+async fn timer(settings: bool, senders: Senders) {
+    println!("Starting timer...");
+    let _ = senders.cli.send("timer".to_string());
 }
 
 #[tokio::main]
@@ -65,40 +77,42 @@ async fn main() {
     let (server_tx, server_rx) = mpsc::channel();
     let (cli_tx, cli_rx) = mpsc::channel();
     let (timer_tx, timer_rx) = mpsc::channel();
-    let connections_server = Senders {
-        cli_sender : Arc::new(cli_tx.clone()),
-        timer_sender : Arc::new(timer_tx.clone()),
-        server_sender : Arc::new(server_tx.clone()),
+    let senders_server = Senders {
+        cli: Arc::new(cli_tx.clone()),
+        timer: Arc::new(timer_tx.clone()),
+        server: Arc::new(server_tx.clone()),
     };
-    let server_to_timer_tx = timer_tx.clone();
-    let server_to_cli_tx = cli_tx.clone();
-    let timer_to_server_tx = server_tx.clone();
-    let timer_to_cli_tx = cli_tx.clone();
+    let senders_timer = senders_server.clone();
     let server = tokio::spawn(async move {
-        block_on(server([127, 0, 0, 1], 8080, connections_server))
+        server([127, 0, 0, 1], 8080, senders_server).await;
     });
     let timer = tokio::spawn(async move {
-        block_on(timer(false, timer_to_server_tx, timer_to_cli_tx, timer_rx))
+        timer(false, senders_timer).await;
     });
 
     let rx = cli_rx;
-    let r1 = rx.recv();
-    let r2 = rx.recv();
+    let _ = rx.recv();
+    let _ = rx.recv();
 
-    while true {
+    loop {
         print!("> ");
         let _ = io::stdout().flush();
         let mut input = String::new();
-        stdin().read_line(&mut input);
+        let _ = stdin().read_line(&mut input);
         println!("{}", input);
         let input = input.trim().to_ascii_lowercase();
         let split = input.split(" ").collect::<Vec<_>>();
-        if split.len() < 1 { continue; }
+        if split.len() < 1 {
+            continue;
+        }
         match split[0] {
+            "stop" => {
+                break;
+            }
             "pause" => {
                 println!("Pausing timer ...");
                 let _ = timer_tx.send("pause".to_string());
-            },
+            }
             "addtime" | "add" => {
                 if split.len() < 2 {
                     continue;
@@ -106,7 +120,12 @@ async fn main() {
                     println!("Adding {} minutes", split[1]);
                 }
             }
-            _ => {println!("unrecognized command \"{}\"", input);}
+            _ => {
+                println!("unrecognized command \"{}\"", input);
+            }
         }
     }
+
+    server.await.expect("");
+    timer.await.expect("");
 }
